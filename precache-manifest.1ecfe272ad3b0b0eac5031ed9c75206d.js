f8f1c971e09025164acace1e3a86fa0f01cb6a4d
Anggairham <anggairham08@gmail.com>
2021-11-05 11:05:52 +0700

    deploy

---
+++ precache-manifest.1ecfe272ad3b0b0eac5031ed9c75206d.js
@@ -0,0 +1,62 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "071c334ffda07b0a739a",
+    "url": "/vue-list-website/css/about.22b7f024.css"
+  },
+  {
+    "revision": "2249e0e3fbf0b3d6ebb7",
+    "url": "/vue-list-website/css/app.c6f9ae7c.css"
+  },
+  {
+    "revision": "7153f1fd9f0e7a623c10",
+    "url": "/vue-list-website/css/chunk-vendors.2ec4bd36.css"
+  },
+  {
+    "revision": "82b9c7a5a3f405032b1db71a25f67021",
+    "url": "/vue-list-website/img/logo.82b9c7a5.png"
+  },
+  {
+    "revision": "d3b171a6f768d4ee9111bc15f11d62fb",
+    "url": "/vue-list-website/index.html"
+  },
+  {
+    "revision": "071c334ffda07b0a739a",
+    "url": "/vue-list-website/js/about.1e68c9fb.js"
+  },
+  {
+    "revision": "2249e0e3fbf0b3d6ebb7",
+    "url": "/vue-list-website/js/app.e060a56e.js"
+  },
+  {
+    "revision": "9db68e666c7893d210be",
+    "url": "/vue-list-website/js/chunk-2d21e3d3.0dd863d2.js"
+  },
+  {
+    "revision": "d0dd941732a6dbc24833",
+    "url": "/vue-list-website/js/chunk-2d21e5ba.3ea39686.js"
+  },
+  {
+    "revision": "39fa2821bb016cf36e03",
+    "url": "/vue-list-website/js/chunk-2d2299f3.5b4c68e7.js"
+  },
+  {
+    "revision": "8e2317cfd43c6ef14eea",
+    "url": "/vue-list-website/js/chunk-6db727e5.6572e6f6.js"
+  },
+  {
+    "revision": "7153f1fd9f0e7a623c10",
+    "url": "/vue-list-website/js/chunk-vendors.bc2c478b.js"
+  },
+  {
+    "revision": "e8ffee4b37780b37178e",
+    "url": "/vue-list-website/js/edit.ca1734fb.js"
+  },
+  {
+    "revision": "e39fad766ad67ff4acca179ba1c5279f",
+    "url": "/vue-list-website/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/vue-list-website/robots.txt"
+  }
+]);
